fbe849db64536441a93c0d280658318c8d735a2a
FhRh <yiyop@naver.com>
2024-08-06 21:20:07 +0900

    test : 특정 위치의 자동차를 한 칸 움직인다

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -1,29 +1,40 @@
 package racingcar.controller
 
 import racingcar.model.Cars
+import racingcar.model.RandomDice
 import racingcar.utils.GameInputer
 import racingcar.view.GameAnnouncer
 
 class GameController {
-    private val gameAnnouncer = GameAnnouncer
-    private val gameInputer = GameInputer
     private val cars = Cars()
     private var rounds = 0
 
     fun ready(){
-        gameAnnouncer.askForCarNames()
-        val carNames = gameInputer.getCarNames()
+        GameAnnouncer.askForCarNames()
+        val carNames = GameInputer.getCarNames()
         cars.initCars(carNames)
 
-        gameAnnouncer.askForRounds()
-        val inputRounds = gameInputer.getRounds()
+        GameAnnouncer.askForRounds()
+        val inputRounds = GameInputer.getRounds()
         rounds = inputRounds
     }
 
     fun process() {
-
+        for (i in 1..rounds) {
+            processRound()
+        }
     }
 
     fun end() {
+    }
+
+    private fun processRound(){
+        val carNum = cars.getSize()
+        for(i in 1..carNum){
+            val diceNum = RandomDice.rollDice()
+            if(diceNum>=4){
+                cars.move(i)
+            }
+        }
     }
 }
--- src/test/kotlin/study/model/CarsTest.kt
+++ src/test/kotlin/study/model/CarsTest.kt
@@ -1,11 +1,13 @@
 package study.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import racingcar.model.Cars
 
 class CarsTest {
     private val cars = Cars()
+
     @Test
     fun `자동차 이름 리스트로 자동차 객체들의 집합을 생성한다`(){
         //given
@@ -22,6 +24,21 @@ class CarsTest {
 
         val resultPositions = cars.getCarList().map{it.position}
         assertEquals(expectedPositions, resultPositions)
+
+    }
+
+    @Test
+    fun `특정 위치의 자동차를 한 칸 움직인다`(){
+        //given
+        val carNames : List<String> = listOf("apple", "banana", "cherry")
+        cars.initCars(carNames)
+
+        //when
+        cars.move(1)
+
+        //then
+        val carList = cars.getCarList()
+        assertEquals(carList[0].position, 1)
 
     }
 }
