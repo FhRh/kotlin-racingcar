4a8fa93df605d99faa09cce8df56d88bd00fa9b7
FhRh <yiyop@naver.com>
2024-08-06 16:56:25 +0900

    test : 라운드 입력과 예외처리에 대한 테스트 코드 작성

--- src/test/kotlin/study/utils/GameInputerTest.kt
+++ src/test/kotlin/study/utils/GameInputerTest.kt
@@ -1,7 +1,9 @@
 package study.utils
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import racingcar.utils.GameInputer
 import java.io.ByteArrayInputStream
 import java.io.InputStream
@@ -10,7 +12,7 @@ import java.io.InputStream
 class GameInputerTest {
     private val gameInputer = GameInputer
     @Test
-    fun `자동차_이름을_입력받아_쉼표_기준으로_구분한다`(){
+    fun `자동차 이름을 입력받아 쉼표 기준으로 구분한다`(){
         //given
         val expected: List<String> = listOf("apple", "banana", "cherry")
         val input = "apple,banana,cherry"
@@ -22,5 +24,33 @@ class GameInputerTest {
 
         //then
         assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `라운드를 입력받는다`(){
+        //given
+        val input = "5"
+        val simulatedInput: InputStream = ByteArrayInputStream(input.toByteArray())
+        System.setIn(simulatedInput)
+
+        //when
+        val result = gameInputer.getRounds()
+
+        //then
+        assertTrue(result is Int,"변수가 정수여야 합니다.")
+    }
+
+    @Test
+    fun `라운드가 잘못 입력될시, IllegalArgumentException을 발생시키고 프로그램을 종료한다`(){
+        //given
+        val input = "종경"
+        val simulatedInput: InputStream = ByteArrayInputStream(input.toByteArray())
+        System.setIn(simulatedInput)
+
+        //when
+        //then
+        assertThrows<IllegalArgumentException> {
+            gameInputer.getRounds()
+        }
     }
 }
