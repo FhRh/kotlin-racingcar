1332d36fe2f1244c2d8014faaef753a5fbb4979f
FhRh <yiyop@naver.com>
2024-08-06 15:26:59 +0900

    feat : 게임 준비 안내문구 출력 코드 작성

---
+++ src/main/kotlin/racingcar/view/GameAnnouncer.kt
@@ -0,0 +1,14 @@
+package racingcar.view
+
+object GameAnnouncer {
+    private const val NAME_INPUT_DESCRIPTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    private const val ROUNDS_INPUT_DESCRIPTION = "시도할 횟수는 몇 회인가요?"
+
+    fun askForCarNames(){
+        println(NAME_INPUT_DESCRIPTION)
+    }
+
+    fun askForRounds() {
+        println(ROUNDS_INPUT_DESCRIPTION)
+    }
+}
