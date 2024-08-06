76ee82c2b581b3e47b63839acb86974f9d0803c0
FhRh <yiyop@naver.com>
2024-08-06 15:25:47 +0900

    feat : 게임 준비 컨트롤러 코드 작성

---
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -0,0 +1,27 @@
+package racingcar.controller
+
+import racingcar.utils.GameInputer
+import racingcar.view.GameAnnouncer
+
+class GameController {
+    private val gameAnnouncer = GameAnnouncer
+    private val gameInputer = GameInputer
+    private val cars = Cars()
+    private var rounds = 0
+
+    fun ready(){
+        gameAnnouncer.askForCarNames()
+        val carNames = gameInputer.getCarNames()
+        cars.initCars(carNames)
+
+        gameAnnouncer.askForRounds()
+        val inputRounds = gameInputer.getRounds()
+        rounds = inputRounds
+    }
+
+    fun process() {
+    }
+
+    fun end() {
+    }
+}
