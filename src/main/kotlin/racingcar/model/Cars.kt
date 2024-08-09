a0631cf01f5b8c40eecb0175b72ef8bc13e22df3
FhRh <yiyop@naver.com>
2024-08-09 17:14:07 +0900

    feat : 입력값 검증 기능 추가

--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -4,6 +4,15 @@ class Cars(private val carList:List<Car>) {
     val size: Int
         get() = carList.size
 
+    init{
+        val nameSet = mutableSetOf<String>()
+        for (car in carList) {
+            if (!nameSet.add(car.name)) {
+                throw IllegalArgumentException("중복된 차 이름이 있습니다 : ${car.name}")
+            }
+        }
+    }
+
     fun getCarList():List<Car>{
         return carList.map{it.copy()}
     }
--- src/main/kotlin/racingcar/view/GameInputer.kt
+++ src/main/kotlin/racingcar/view/GameInputer.kt
@@ -8,6 +8,11 @@ object GameInputer {
 
     fun getRounds() : Int{
         val input = readlnOrNull()
-        return input?.toIntOrNull() ?: throw IllegalArgumentException("유효한 정수를 입력하지 않았습니다.")
+        val number = input?.toIntOrNull() ?: throw IllegalArgumentException("유효한 정수를 입력하지 않았습니다.")
+
+        if (number < 0) {
+            throw IllegalArgumentException("음수는 허용되지 않습니다.")
+        }
+        return number
     }
 }
